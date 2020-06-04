300f1d3efb0e82586779ebf279d95db469d338d8
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-06-03 19:36:58 -0500

    Add files via upload
    Testing to see if I can run multiple test, it can be wrong and right depending on my values.

--- test_functions.py
+++ test_functions.py
@@ -6,6 +6,10 @@ class MyTestCase(unittest.TestCase):
     def test_convert_to_months(self):
         self.assertEqual(convert_to_months(5), 60)
 
+    def test_convert_to_months1(self):
+        self.assertEqual(convert_to_months(2), 25)
+
+
 
 if __name__ == '__main__':
     unittest.main()
