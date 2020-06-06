aaddbbe73db122ad0e2419c20a4aff9a6a563d60
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-06-06 08:45:13 -0500

    Add files via upload

--- MOD3/TOP2/format_output/average_scores.py
+++ MOD3/TOP2/format_output/average_scores.py
@@ -1,2 +1,5 @@
 def average():
-    pass
+    input_scores = input('Please enter the three scores '
+                         'separated by a space: ')
+    scores = [int(i) for i in (input_scores.split())]
+    return sum(scores) / len(scores)
--- MOD3/TOP2/test/test_average_scores.py
+++ MOD3/TOP2/test/test_average_scores.py
@@ -1,12 +1,13 @@
 import unittest
 from unittest import mock
+
 from TOP2.format_output.average_scores import average
 
 
-def test_average():
-    with mock.patch('builtins.input', side_effect=[85, 90, 95]):
-        assert average() == 90
+class MyTestCase(unittest.TestCase):
+    def test_average(self):
+        self.assertEqual(average(), 90)
 
 
 if __name__ == '__main__':
-    test_average()
+    unittest.main()
